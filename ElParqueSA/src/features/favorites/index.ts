a20ec089aa418f0792b8da49156a99a3b9a52da3
Ecruz1997 <everardocruzjime@gmail.com>
2026-04-20 21:01:50 -0600

    Se borra Tarea1

--- ElParqueSA/src/features/favorites/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './components/FavoriteCard';
-export * from './components/FavoriteList';
-export * from './containers/FavoriteListContainer';
-export * from './pages/FavoritesPage';
-export * from './types';
-export * from './utils/favoriteUtils';
--- ElParqueSA/src/features/recipes/types.ts
+++
@@ -1,7 +0,0 @@
-export interface Recipe {
-  id: string;
-  title: string;
-  descriptionIngredientes: string;
-  descriptionPreparacion: string;
-  ilustracion?: string;
-}
